e7027f77888c6566ba2d950804b15ff8ecc09a64
cibo-24 <cibo.kenan@gmail.com>
2024-03-21 20:39:05 +0300

    update

---
+++ admin-dashboard/src/scenes/dashboard/index.jsx
@@ -0,0 +1,16 @@
+// her şeyi temsil eden component
+import React from 'react';
+import Header from '../../components/Header';
+import { Box } from '@mui/material';
+
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+    <Header title="Dashboard" subtitle="Welcome to your dashboard!"/>
+    </Box>
+    </Box>
+  )
+}
+
+export default Dashboard;
